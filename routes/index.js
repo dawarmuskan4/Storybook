f2a5fa6dd4fe779dae2bbaddcd13684aa04182da
Muskan Dawar <sanchitdawar34@gmail.com>
2021-08-08 12:33:04 +0530

    first

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+// Login/Landing page
+// @route GET/
+router.get('/', (req,res) => {
+  res.render('login', {
+    layout: 'login',
+  });
+})
+
+// dashboard
+// @route GET/dashboard
+router.get('/dashboard', (req,res) => {
+  res.render('dashboard')
+})
+
+module.exports = router

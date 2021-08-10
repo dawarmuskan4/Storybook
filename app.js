1a82f535288205058168618e3cf6c8df0385bb45
Muskan Dawar <sanchitdawar34@gmail.com>
2021-08-10 21:47:19 +0530

    second

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 //Load config
@@ -17,25 +21,65 @@ connectDB()
 
 const app = express()
 
+//cors
+app.use(cors())
+
+//Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+//Method override
+app.use(methodOverride(function (req,res) {
+  if(req.body && typeof req.body === 'object' && '_method' in req.body) {
+    //look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 //Logging
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'))
 }
 
+//Handlebars Helpers
+const {formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+
 //handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({helpers: {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select
+},
+  defaultLayout: 'main',
+  extname: '.hbs'
+  })
+);
 app.set('view engine', '.hbs');
 
 //Sessions
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-}))
+
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongoUrl : process.env.MONGO_URI }),
+  })
+)
 
 //Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+//set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -43,6 +87,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories') )
 
 const PORT = process.env.PORT || 3000
 
